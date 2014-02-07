dc45c8812217194f5b36c211cc7bf3ff028f2249
Hari B <linkwithhari@gmail.com>
2014-02-08 00:08:07 +0530

    initial checkin

---
+++ Module/CategoryService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Nancy;
+
+namespace nancy_sample.Module
+{
+    public class CategoryService : NancyModule
+    {
+
+        public CategoryService() : base("/services")
+        {
+            Get["/categories"] = parameters =>
+            {
+                return Response.AsJson(Categories.all());
+            };
+        }
+
+    }
+
+    public class Categories
+    {
+        public static List<Category> allCategories = new List<Category>(){new Category(1, "tactial"),new Category(2,"strategic"), new Category(3, "staff aug")};
+
+
+
+        public static List<Category> all()
+        {
+            return allCategories;
+        }
+    }
+
+    public class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
